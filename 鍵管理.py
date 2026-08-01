ff66e5250267d21b9633bc5a667d6c144c7194d6
yutaki <yuuki.f.tmy2a@gmail.com>
2026-08-01 18:33:43 +0900

    Update 鍵管理.py

--- 鍵管理.py
+++ 鍵管理.py
@@ -1,13 +1,14 @@
 from Crypto.PublicKey import RSA
 
+
 def generate_key_pair(key_size: int = 2048):
     key = RSA.generate(key_size)
     
-    get_private_key_pem = key.export_key().decode("utf-8")
+    get_private_key = key.export_key().decode("utf-8")
     
-    get_public_key_pem = key.publickey().export_key().decode("utf-8")
+    get_public_key = key.publickey().export_key().decode("utf-8")
     
-    return get_private_key_pem, get_public_key_pem
+    return get_private_key, get_public_key
 
 def load_private_key(pem_string: str):
     return RSA.import_key(pem_string)
